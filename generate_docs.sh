2ed7a2bded1def89eb4a2b7c97f5090b5fcbdf07
mcgillij <mcgillivray.jason@gmail.com>
2021-07-22 22:29:03 -0300

    remove old files for python2.7

--- generate_docs.sh
+++
@@ -1,1 +0,0 @@
-python /c/Python27/Scripts/epydoc.py --html -o ../epy -vvvv Game.py CharCreator.py ColorPicker.py CombatLog.py Cursors.py Job.py JobList.py MapTile.py MonsterGenerator.py Player.py Room.py Stats.py
